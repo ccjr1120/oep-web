e8f704baf4d3871503ef01dca894bac0cca5677b
ccjr <ccjr1120@163.com>
2021-03-07 14:04:18 +0800

    ant+react+ts的简单尝试

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React, { FC } from 'react';
-import { Button } from 'antd';
-import './App.css';
+import React, { FC } from "react";
+import { Layout } from "antd";
+import "./App.css";
+
+const { Header, Sider, Content } = Layout;
+const layoutStyle: React.CSSProperties = {
+  height: "100%",
+};
 
 const App: FC = () => (
   <div className="App">
-    <Button type="primary">Button</Button>
+    <Layout style={layoutStyle}>
+      <Sider collapsed={true}>Sider</Sider>
+      <Layout>
+        <Header>Header</Header>
+        <Content>Content</Content>
+      </Layout>
+    </Layout>
   </div>
 );
 
